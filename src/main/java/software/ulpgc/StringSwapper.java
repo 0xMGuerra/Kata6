7c9c36d37249d65a92bd5c1e67a6a9140751352b
Micael <mguerrargz@gmail.com>
2024-01-20 16:41:36 +0000

    Modified swapFirstAndLastChar to accept empty strings or one char string and multiple lenghts.

--- src/main/java/software/ulpgc/StringSwapper.java
+++ src/main/java/software/ulpgc/StringSwapper.java
@@ -3,10 +3,19 @@ package software.ulpgc;
 public class StringSwapper {
 
     public String swapFirstAndLastChar(String s) {
+
+        int size = size(s);
+
+        if(size <= 1) return s;
+
         char firstChar = s.charAt(0);
-        char lastChar = s.charAt(3);
-        String middleChars = s.substring(1,3);
+        char lastChar = s.charAt(size - 1);
+        String middleChars = s.substring(1, size - 1);
 
         return lastChar + middleChars + firstChar;
+    }
+
+    public int size(String s) {
+        return s.length();
     }
 }
--- src/test/java/StringSwapperTest.java
+++ src/test/java/StringSwapperTest.java
@@ -11,4 +11,19 @@ public class StringSwapperTest {
         String test = "abcd";
         assertEquals("dbca", stringSwapper.swapFirstAndLastChar(test));
     }
+
+    @Test
+    public void swap_first_char_with_last_char_given_empty_string() {
+        StringSwapper stringSwapper = new StringSwapper();
+        String test = "";
+        assertEquals("", stringSwapper.swapFirstAndLastChar(test));
+    }
+
+    @Test
+    public void swap_first_char_with_last_char_given_one_char_string() {
+        StringSwapper stringSwapper = new StringSwapper();
+        String test = "a";
+        assertEquals("a", stringSwapper.swapFirstAndLastChar(test));
+
+    }
 }
